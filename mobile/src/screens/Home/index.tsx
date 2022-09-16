dc991f41beb13e7b3ce6bb3cbbeb9b2112551fd4
DioneDw <dionewilly@hotmail.com>
2022-09-16 01:05:43 -0300

    second-class-complete

---
+++ mobile/src/screens/Home/index.tsx
@@ -0,0 +1,33 @@
+import { View, Image, FlatList } from 'react-native';
+
+import { styles } from './styles';
+import logoImg from '../../assets/logo-nlw-esports.png';
+import { Heading } from '../../components/Heading';
+import { GameCard } from '../../components/Gamecard';
+import { GAMES } from '../../utils/games';
+
+export function Home() {
+  return (
+    <View style={styles.container}>
+      <Image source={logoImg} style={styles.logo} />
+      
+      <Heading 
+      title="Encontre seu duo!"
+      subtitle="Selecione o game que deseja jogar..."
+       />
+
+      <FlatList 
+      data={GAMES} 
+      keyExtractor={ item => item.id}
+      renderItem= {({ item }) => (
+      <GameCard 
+       data={item} 
+       /> 
+      )}
+      showsHorizontalScrollIndicator={false}
+      horizontal
+      contentContainerStyle={styles.contentList}
+      />
+    </View>
+  );
+}
