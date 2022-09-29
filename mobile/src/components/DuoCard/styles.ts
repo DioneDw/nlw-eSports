ea339cc12d3e594f7de46d130019f26300a5b0f9
DioneDw <dionewilly@hotmail.com>
2022-09-28 22:56:49 -0300

    fourth-class-mobile-duocard-complete

--- mobile/src/components/DuoCard/styles.ts
+++ mobile/src/components/DuoCard/styles.ts
@@ -3,10 +3,25 @@ import { THEME } from '../../theme';
 
 export const styles = StyleSheet.create({
   container: {
-    width: 180,
+    width: 200,
     backgroundColor: THEME.COLORS.SHAPE,
     borderRadius: 8,
     padding: 20,
     marginRight: 16
+  },
+  button: {
+    width: 140,
+    height: 36,
+    borderRadius: 3,
+    backgroundColor: THEME.COLORS.PRIMARY,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  buttonTitle: {
+    color: THEME.COLORS.TEXT,
+    fontFamily: THEME.FONT_FAMILY.SEMI_BOLD,
+    fontSize: THEME.FONT_SIZE.SM,
+    marginLeft: 8
   }
 });
--- mobile/src/screens/Game/index.tsx
+++ mobile/src/screens/Game/index.tsx
@@ -1,26 +1,33 @@
 
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { Background } from '../../components/Background';
 import { useNavigation, useRoute } from '@react-navigation/native';
 import { styles } from './styles';
 import { GameParams } from '../../@types/navigation';
-import { Image, TouchableOpacity, View} from 'react-native';
+import { FlatList, Image, Text, TouchableOpacity, View} from 'react-native';
 import { Entypo } from '@expo/vector-icons';
 import { THEME } from '../../theme';
 import logoImg from '../../assets/logo-nlw-esports.png'
 import { Heading } from '../../components/Heading';
-import { DuoCard } from '../../components/DuoCard';
-
+import { DuoCard, DuoCardProps } from '../../components/DuoCard';
 export function Game() {
+  const [duos, setDuos] = useState<DuoCardProps[]>([])
+
+
   const navigation = useNavigation();
   const route = useRoute();
   const game = route.params as GameParams;
-
   function handleGoBack(){
     navigation.goBack()
   }
-
+  
+  useEffect(()=> {
+  fetch(`http://192.168.2.5:3333/games/${game.id}/ads`)
+  .then(response => response.json())
+  .then(data => setDuos(data));
+  },[]);
+  
 
   return (
     <Background>
@@ -47,10 +54,28 @@ export function Game() {
      />
      <Heading
      title={game.name}
-     subtitle='Conecte-se e começe a jogar!'>
-      
-     </Heading>
-     <DuoCard />
+     subtitle='Conecte-se e começe a jogar!'
+     />   
+     <FlatList
+     data = {duos}
+     keyExtractor = {item => item.id}
+     renderItem = {({item}) =>
+     (
+      <DuoCard 
+      data= {item}
+      onConnect= {()=> {}}
+      />
+     )}
+     horizontal
+     style = {styles.containerList}
+     contentContainerStyle= {[duos.length>0?styles.contentList:styles.emptyListContent]}
+     showsHorizontalScrollIndicator={false}
+     ListEmptyComponent = {()=> (
+      <Text style={styles.emptyListText}>
+        Não há anúncios publicados ainda.
+      </Text>
+     )}
+     />
     </SafeAreaView>
     </Background>
   );
