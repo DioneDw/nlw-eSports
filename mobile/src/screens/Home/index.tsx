e72c11bbfc15f1c82b8583610a295e89c6f35a5a
DioneDw <dionewilly@hotmail.com>
2022-10-06 23:08:24 -0300

    project-preview

--- mobile/src/screens/Home/index.tsx
+++ mobile/src/screens/Home/index.tsx
@@ -13,7 +13,7 @@ export function Home() {
   const [games,setGames] = useState<GameCardProps[]>([]);
 
   useEffect(() => {
-    fetch('http://192.168.2.8:3333/games ')
+    fetch('192.168.2.4:3333/games')
     .then(response => response.json())
     .then(data => setGames(data))
   }, []);
